be886739760d59ba5db7ee1cf37a492ed6c9d20b
Adir207 <adir.segal@ruppin365.net>
2021-05-18 14:18:28 +0300

    Commit on app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/', (req, res, next) => {
-	res.send('welcome to Ruppin Regfree Server');
+	res.send('welcome to Ruppin Regfree Server by Cloudonix');
 });
 
 app.listen(3000);
