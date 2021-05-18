3d2d887277c8b0ed85be1259d43ed9d68cbb56bd
levshpilko <levs101@gmail.com>
2021-05-18 14:12:10 +0300

    add basic sever home route

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+	res.send('welcome to Ruppin Regfree Server');
+});
+
+app.listen(3000);
